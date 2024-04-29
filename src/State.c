6ea14e5ff716d7413546a879ac11df26fbdd2360
Ellyzeul <gabrielaug.240@gmail.com>
2024-04-29 00:01:58 -0300

    feat: jogador iniciando no meio

--- src/State.c
+++ src/State.c
@@ -2,10 +2,6 @@
 #include "Constants.h"
 #include <raylib.h>
 
-float playerPositionProportion(float sizeProportion) {
-  return (1 - sizeProportion) / 2;
-}
-
 State stateInit(void) {
   State state;
 
@@ -20,7 +16,7 @@ State stateInit(void) {
   state.scoreSizeProportion = SCORE_SIZE_PROPORTION;
 
   state.player1.sizeProportion = PLAYER_SIZE_PROPORTION;
-  state.player1.positionProportion = playerPositionProportion(state.player1.sizeProportion);
+  state.player1.positionProportion = PLAYER_POSITION_PROPORTION;
   state.player1.velocity = PLAYER_VELOCITY;
   state.player1.upButton = KEY_W;
   state.player1.downButton = KEY_S;
@@ -30,7 +26,7 @@ State stateInit(void) {
   state.player1.dashCooldown = 0.0f;
 
   state.player2.sizeProportion = PLAYER_SIZE_PROPORTION;
-  state.player2.positionProportion = playerPositionProportion(state.player2.sizeProportion);
+  state.player2.positionProportion = PLAYER_POSITION_PROPORTION;
   state.player2.velocity = PLAYER_VELOCITY;
   state.player2.upButton = KEY_UP;
   state.player2.downButton = KEY_DOWN;
